4f5867e4e8888cd974036acbd5578b858062dba1
Sivanesh Shanmugam <sivaneshraina@gmail.com>
2021-01-05 02:50:08 +0530

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+
 function Header() {
-  return <div></div>;
+  return (
+    <div style={{ padding: '30px', margin: '30px' }}>
+      <Title>Select Playing 9</Title>{' '}
+    </div>
+  );
 }
 
 export default Header;
